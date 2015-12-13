a07ddfd53548a934d44f547aacc9eaed4446da5f
Magnus Nordseth <magnus.nordseth@silabs.com>
2015-12-13 23:27:53 +0100

    Add distance to statusbar

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -10,16 +10,34 @@ export default class StatusBar extends Component {
     const { activity } = this.props;
     const state = activity.get('state');
     const start = activity.get('start');
+    const dist = Math.round((activity.get('lwheeldist') + activity.get('rwheeldist')) / 2 / 1000);
+
     let end = activity.get('end');
     if (!end) {
       end = Date.now() / 1000;
     }
-    const duration = end - start;
+    let duration = end - start;
+    const hours = Math.floor(duration / 3600);
+    duration = Math.round(duration % 3600);
     const min = Math.floor(duration / 60);
     const sec = Math.round(duration % 60);
+    let timestamp = '';
+
+    if (hours > 0) {
+      timestamp = timestamp + hours + ' hours ';
+    }
+    timestamp = timestamp + min + ' min ' + sec + ' sec';
+
+    let distance = '';
+    if (dist > 0) {
+      distance = ' | ' + dist + ' meters';
+    }
+
     return (
       <Paper zDepth={5} className={styles.parent}>
-        <p>{state} for {min}min {sec} secs</p>
+        <p>
+          {state} for {timestamp} {distance}
+        </p>
       </Paper>
     );
   }
